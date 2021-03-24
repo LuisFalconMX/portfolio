de9f457987f22287402cd7725a715ae941502ac6
luisfalconmx <me@luisfalconmx.com>
2021-03-24 17:58:34 -0600

    feat: ProjectCard component created with minimal configuration

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Jumbotron from '@components/Jumbotron'
+import ProjectCard from '@components/ProjectCard'
 
 const Projects = () => (
   <>
     <Jumbotron />
+    <div className="grid grid-cols-3 gap-1 mt-16 px-2">
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
+    </div>
   </>
 )
 
