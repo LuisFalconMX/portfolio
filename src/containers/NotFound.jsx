07085dc2a59d38581cc301e89c5dc54a9ec0fad7
luisfalconmx <me@luisfalconmx.com>
2021-03-25 22:12:09 -0600

    style: NotFound component updated with more styles

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Button from '@components/Button'
+import { Home } from 'react-feather'
+import '@styles/components/NotFound.pcss'
 
 const NotFound = () => (
   <>
-    <h1>404 Not Found</h1>
+    <div className="NotFound">
+      <h1 className="NotFound__title">404</h1>
+      <h2 className="NotFound__subtitle">No se ha encontrado la pagina que solicitaste</h2>
+      <Button message="Regresar al sitio" navigation="/" icon={<Home />} />
+    </div>
   </>
 )
 
